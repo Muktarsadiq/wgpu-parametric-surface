cbcf392074331c50ce91ea5f0f1891a2a156dbfb
jack1232 <jackjack5810@yahoo.com>
2023-09-04 11:59:08 -0400

    update to 0.17

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,32 @@
 mod common;
 mod math_func;
+#[path="surface_data.rs"]
+mod sd;
+use std::f32::consts::PI;
 
 fn main(){
-    let mut colormap_name = "jet";
-    let mut is_two_side:i32 = 1;
+    let mut function_selection = 0;
     let args: Vec<String> = std::env::args().collect();
-    if args.len() > 1 {
-        colormap_name = &args[1];
+    if args.len() > 1{
+        function_selection = args[1].parse().unwrap();
     }
-    if args.len() > 2 {
-        is_two_side = args[2].parse().unwrap();
-    }    
 
-    let vertex_data = common::create_vertices(&math_func::peaks, colormap_name, -3.0, 3.0, -3.0, 3.0, 
-        51, 51, 2.0, 1.0);
-    let light_data = common::light([1.0, 1.0, 1.0], 0.1, 0.8, 0.4, 30.0, is_two_side);
-    common::run(&vertex_data, light_data, colormap_name, "peaks");
+    let ps_struct:sd::ParametricSurface;
+
+    if function_selection == 1 {
+        ps_struct = sd::ParametricSurface {f:math_func::klein_bottle, umin:0.0, umax:PI, vmin:0.0, vmax:2.0*PI,
+            u_segments:120, v_segments:40, scale: 1.0,..Default::default()};
+    } else if function_selection == 2 {
+        ps_struct = sd::ParametricSurface{f:math_func::wellenkugel, umin:0.0, umax:14.5, vmin:0.0, vmax:5.0,
+            u_segments:100, v_segments:50, scale:0.17, colormap_name:"cool", ..Default::default()};
+    } else {
+        ps_struct = sd::ParametricSurface{..Default::default()};
+    }
+
+    let (pos_data, normal_data, color_data, index_data) 
+        = sd::ParametricSurface::new(ps_struct);
+
+    let light_data = common::light([1.0, 1.0, 1.0], 0.1, 0.8, 0.4, 30.0, 1);
+
+    common::run(&pos_data, &normal_data, &color_data, &index_data, light_data);
 }
--- src/math_func.rs
+++ src/math_func.rs
@@ -1,12 +1,34 @@
 #![allow(dead_code)]
 use cgmath::*;
 
+pub fn klein_bottle(u:f32, v:f32, _params:[f32;5]) -> [f32; 3] {
+    let x = 2.0/15.0*(3.0+5.0*u.cos()*u.sin())*v.sin(); 
+
+    let y = -1.0/15.0*u.sin()*(3.0*v.cos()-3.0*(u.cos()).powf(2.0)*v.cos()-
+        48.0*(u.cos()).powf(4.0)*v.cos()+48.0*(u.cos()).powf(6.0)*v.cos()-
+        60.0*u.sin()+5.0*u.cos()*v.cos()*u.sin()-5.0*(u.cos()).powf(3.0)*v.cos()*u.sin()-
+        80.0*(u.cos()).powf(5.0)*v.cos()*u.sin()+80.0*(u.cos()).powf(7.0)*v.cos()*u.sin())-2.0;
+
+    let z = -2.0/15.0*u.cos()*(3.0*v.cos()-30.0*u.sin() +
+        90.0*(u.cos()).powf(4.0)*u.sin()-60.0*(u.cos()).powf(6.0)*u.sin() + 5.0*u.cos()*v.cos()*u.sin());
+
+    [x, y, z]
+}
+
+pub fn wellenkugel(u:f32, v:f32, _params:[f32;5]) -> [f32; 3] {
+    let x = u*(u.cos()).cos()*v.sin();        
+    let y = u*(u.cos()).sin();
+    let z = u*(u.cos()).cos()*v.cos();    
+    [x, y, z]
+}
+
 pub fn torus(u:f32, v:f32, params:[f32;5]) -> [f32; 3] {
     let x = (params[0]+params[1]*v.cos())*u.cos();
     let y = params[1]*v.sin();
     let z = (params[0]+params[1]*v.cos())*u.sin();
     [x, y, z]
 }
+
 
 pub fn peaks (x:f32, z:f32) -> [f32; 3] {
     let y = 3.0*(1.0-x)*(1.0-x)*(-(x*x)-(z+1.0)*(z+1.0)).exp()-
